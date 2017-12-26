278613a069b47beefff5e06e533795e45551de16
Annie Chiang <devbear22@gmail.com>
2017-12-26 14:54:22 -0800

    change to mapDispatchToState with constructor to bind this, not working - props.increment is not a function

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import { incrementAction, decrementAction } from '../Actions';
+import { incrementAction, decrementAction } from '../Actions';
 import { INCREMENT, DECREMENT } from '../Actions';
 
 export class Counter extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //   this.increment = this.increment.bind(this);
-  //   this.decrement = this.decrement.bind(this);
-  // }
+  constructor(props){
+    super(props);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
 
   // why is it not hitting the reducers to update the state? 
-  // increment() {
-  //   console.log('increment ->', this.props);
-  //   this.props.increment();
-  // }
-
-  // decrement() {
-  //   console.log('decrement ->', this.props);
-  //   this.props.decrement();
-  // }
-
-  // // without mapDispatchToProps: 
-  increment = () => {
-    console.log('increment props', this.props);
-    this.props.dispatch({ type: INCREMENT });
+  increment() {
+    console.log('increment ->', this.props);
+    this.props.increment();
   }
 
-  decrement = () => {
-    this.props.dispatch({ type: DECREMENT });
+  decrement() {
+    console.log('decrement ->', this.props);
+    this.props.decrement();
   }
+
+  // without mapDispatchToProps: 
+  // increment = () => {
+  //   console.log('increment props', this.props);
+  //   this.props.dispatch({ type: INCREMENT });
+  // }
+
+  // decrement = () => {
+  //   this.props.dispatch({ type: DECREMENT });
+  // }
 
   render() {
     return (
@@ -51,17 +51,17 @@ const mapStateToProps = (state) => {
   };
 }
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     increment() {
-//       const action = incrementAction();
-//       dispatch(action);
-//     },
-//     decrement() {
-//       const action = decrementAction();
-//       dispatch(action);
-//     }
-//   }
-// }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment() {
+      const action = incrementAction();
+      dispatch(action);
+    },
+    decrement() {
+      const action = decrementAction();
+      dispatch(action);
+    }
+  }
+}
 
 export default connect(mapStateToProps)(Counter);
